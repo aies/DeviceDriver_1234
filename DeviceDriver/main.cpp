853aa09a5743c6e31974aec8e39c15c0eaeb9a38
ies.cho <ies.cho@sec.com>
2025-07-24 15:39:20 +0900

    Read exception

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,13 +1,25 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
+using namespace testing;
+
+class Flash_Mock : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
 TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
+	Flash_Mock mockFlash;
+
+	EXPECT_CALL(mockFlash, read(0x8))
+		.Times(5)
+		.WillRepeatedly(Return(0x8));
+
+	DeviceDriver driver{ &mockFlash };
+	int data = driver.read((long)0x8);
 }
+
 
 int main() {
 	::testing::InitGoogleMock();
